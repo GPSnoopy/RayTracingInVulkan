17ddc303305135d18ab7ae61c7ccc69999fedca6
Tanguy Fautre <tanguy@fautre.com>
2019-04-06 20:44:03 +0100

    Fixed Linux compilation

--- src/Vulkan/Enumerate.hpp
+++ src/Vulkan/Enumerate.hpp
@@ -6,34 +6,6 @@
 namespace  Vulkan {
 
 template <class TValue>
-inline std::vector<TValue> GetEnumerateVector(VkResult(enumerate) (uint32_t*, TValue*))
-{
-	std::vector<TValue> initial;
-	return GetEnumerateVector(enumerate, initial);
-}
-
-template <class THandle, class TValue>
-inline std::vector<TValue> GetEnumerateVector(THandle handle, void(enumerate) (THandle, uint32_t*, TValue*))
-{
-	std::vector<TValue> initial;
-	return GetEnumerateVector(handle, enumerate, initial);
-}
-
-template <class THandle, class TValue>
-inline std::vector<TValue> GetEnumerateVector(THandle handle, VkResult(enumerate) (THandle, uint32_t*, TValue*))
-{
-	std::vector<TValue> initial;
-	return GetEnumerateVector(handle, enumerate, initial);
-}
-
-template <class THandle1, class THandle2, class TValue>
-inline std::vector<TValue> GetEnumerateVector(THandle1 handle1, THandle2 handle2, VkResult(enumerate) (THandle1, THandle2, uint32_t*, TValue*))
-{
-	std::vector<TValue> initial;
-	return GetEnumerateVector(handle1, handle2, enumerate, initial);
-}
-
-template <class TValue>
 inline std::vector<TValue> GetEnumerateVector(VkResult(enumerate) (uint32_t*, TValue*), std::vector<TValue>& vector)
 {
 	uint32_t count = 0;
@@ -85,6 +57,34 @@ inline std::vector<TValue> GetEnumerateVector(THandle1 handle1, THandle2 handle2
 		"enumerate");
 
 	return vector;
+}
+
+template <class TValue>
+inline std::vector<TValue> GetEnumerateVector(VkResult(enumerate) (uint32_t*, TValue*))
+{
+	std::vector<TValue> initial;
+	return GetEnumerateVector(enumerate, initial);
+}
+
+template <class THandle, class TValue>
+inline std::vector<TValue> GetEnumerateVector(THandle handle, void(enumerate) (THandle, uint32_t*, TValue*))
+{
+	std::vector<TValue> initial;
+	return GetEnumerateVector(handle, enumerate, initial);
+}
+
+template <class THandle, class TValue>
+inline std::vector<TValue> GetEnumerateVector(THandle handle, VkResult(enumerate) (THandle, uint32_t*, TValue*))
+{
+	std::vector<TValue> initial;
+	return GetEnumerateVector(handle, enumerate, initial);
+}
+
+template <class THandle1, class THandle2, class TValue>
+inline std::vector<TValue> GetEnumerateVector(THandle1 handle1, THandle2 handle2, VkResult(enumerate) (THandle1, THandle2, uint32_t*, TValue*))
+{
+	std::vector<TValue> initial;
+	return GetEnumerateVector(handle1, handle2, enumerate, initial);
 }
 
 }
