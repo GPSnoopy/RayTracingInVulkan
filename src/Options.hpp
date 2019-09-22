284ed744071653653bfde4bbf908976220d98583
Tanguy Fautre <tanguy@fautre.com>
2019-09-22 20:22:55 +0100

    Fixed warnings

--- src/Options.hpp
+++ src/Options.hpp
@@ -22,12 +22,12 @@ public:
 	bool Benchmark{};
 
 	// Scene options.
-	uint32_t SceneIndex;
+	uint32_t SceneIndex{};
 
 	// Renderer options.
-	uint32_t Samples;
-	uint32_t Bounces;
-	uint32_t MaxSamples;
+	uint32_t Samples{};
+	uint32_t Bounces{};
+	uint32_t MaxSamples{};
 
 	// Window options
 	uint32_t Width{};
