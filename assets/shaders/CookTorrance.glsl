125feca96883fb38915de01efa96a0de19c3171d
Ruth Holness <holnesruth@myvuw.ac.nz>
2019-12-06 13:44:46 +1300

    Split cook torrance shading into separate file

---
+++ assets/shaders/CookTorrance.glsl
@@ -0,0 +1,37 @@
+#define PI 3.14159265
+
+vec3 CookTorrance(vec3 diffuse, vec3 normal, vec3 lightDir, vec3 viewDir, float roughness) {
+	float F0 = 0.8;
+	float k = 0.2;
+
+	float NdotL = max(0, dot(normal, lightDir));
+	float Rs = 0.0;
+	if (NdotL > 0) 
+	{
+		vec3 H = normalize(lightDir + viewDir);
+		float NdotH = max(0, dot(normal, H));
+		float NdotV = max(0, dot(normal, viewDir));
+		float VdotH = max(0, dot(lightDir, H));
+
+		// Fresnel reflectance
+		float F = pow(1.0 - VdotH, 5.0);
+		F *= (1.0 - F0);
+		F += F0;
+
+		// Microfacet distribution by Beckmann
+		float m_squared = roughness * roughness;
+		float r1 = 1.0 / (4.0 * m_squared * pow(NdotH, 4.0));
+		float r2 = (NdotH * NdotH - 1.0) / (m_squared * NdotH * NdotH);
+		float D = r1 * exp(r2);
+
+		// Geometric shadowing
+		float two_NdotH = 2.0 * NdotH;
+		float g1 = (two_NdotH * NdotV) / VdotH;
+		float g2 = (two_NdotH * NdotL) / VdotH;
+		float G = min(1.0, min(g1, g2));
+
+		Rs = (F * D * G) / (PI * NdotL * NdotV);
+	}
+
+	return (diffuse * NdotL) + (NdotL * (k + Rs * (1.0 - k)));
+}
