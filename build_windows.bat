e883ddff39dcb9b62053e7166647462f46e02ef1
Ruth Holness <holnesruth@myvuw.ac.nz>
2019-12-10 11:58:33 +1300

    Added tessellation shader files with Sascha Willems' code for a starting point

--- build_windows.bat
+++ build_windows.bat
@@ -2,6 +2,7 @@ cd build || goto :error
 mkdir windows
 :: || goto :error
 cd windows || goto :error
+set PATH="C:\Program Files\CMake\bin\";%PATH%
 cmake -D VCPKG_TARGET_TRIPLET=x64-windows-static -D CMAKE_TOOLCHAIN_FILE=../vcpkg.windows/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 16 2019" -A "x64" ../.. || goto :error
 :: msbuild RayTracingInVulkan.sln /t:Rebuild /p:Configuration=Release || goto :error
 cd ..
