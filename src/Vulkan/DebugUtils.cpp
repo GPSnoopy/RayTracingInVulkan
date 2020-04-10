27a6323f71973f528a98c0daa40d7ccacfb1907c
Tanguy Fautre <tanguy@fautre.com>
2020-04-10 14:57:25 +0100

    Fix Release runtime error.

--- src/Vulkan/DebugUtils.cpp
+++ src/Vulkan/DebugUtils.cpp
@@ -6,10 +6,12 @@ namespace Vulkan {
 DebugUtils::DebugUtils(VkInstance instance)
 	: vkSetDebugUtilsObjectNameEXT_(reinterpret_cast<PFN_vkSetDebugUtilsObjectNameEXT>(vkGetInstanceProcAddr(instance, "vkSetDebugUtilsObjectNameEXT")))
 {
+#ifndef NDEBUG
 	if (vkSetDebugUtilsObjectNameEXT_ == nullptr)
 	{
 		Throw(std::runtime_error("failed to get address of 'vkSetDebugUtilsObjectNameEXT'"));
 	}
+#endif
 }
 
 }
