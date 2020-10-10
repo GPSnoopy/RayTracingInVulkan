8eaca7712f83d2c97d1e8dc2559ffc4a145232a9
Tanguy Fautre <tanguy@fautre.com>
2020-10-10 21:50:35 +0100

    Use slightly faster, but not as good, float rand() function.

--- assets/shaders/Random.glsl
+++ assets/shaders/Random.glsl
@@ -1,7 +1,8 @@
 #extension GL_EXT_control_flow_attributes : require
 
 // Generates a seed for a random number generator from 2 inputs plus a backoff
-// https://github.com/nvpro-samples/optix_prime_baking/blob/master/random.h
+// https://github.com/nvpro-samples/optix_prime_baking/blob/332a886f1ac46c0b3eea9e89a59593470c755a0e/random.h
+// https://github.com/nvpro-samples/vk_raytracing_tutorial_KHR/tree/master/ray_tracing_jitter_cam
 // https://en.wikipedia.org/wiki/Tiny_Encryption_Algorithm
 uint InitRandomSeed(uint val0, uint val1)
 {
@@ -26,10 +27,13 @@ uint RandomInt(inout uint seed)
 
 float RandomFloat(inout uint seed)
 {
-	// Float version using bitmask from Numerical Recipes
-	const uint one = 0x3f800000;
-	const uint msk = 0x007fffff;
-	return uintBitsToFloat(one | (msk & (RandomInt(seed) >> 9))) - 1;
+	//// Float version using bitmask from Numerical Recipes
+	//const uint one = 0x3f800000;
+	//const uint msk = 0x007fffff;
+	//return uintBitsToFloat(one | (msk & (RandomInt(seed) >> 9))) - 1;
+
+	// Faster version from NVIDIA examples; quality good enough for our use case.
+	return (float(RandomInt(seed) & 0x00FFFFFF) / float(0x01000000));
 }
 
 vec2 RandomInUnitDisk(inout uint seed)
