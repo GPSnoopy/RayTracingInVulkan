55d6e1534679ce218d5df71160c33696af232416
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-04-23 21:16:28 +0300

    vcpkg version updated to 2023.04.15
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- vcpkg_linux.sh
+++ vcpkg_linux.sh
@@ -5,7 +5,7 @@ mkdir -p build
 cd build
 git clone https://github.com/Microsoft/vcpkg.git vcpkg.linux
 cd vcpkg.linux
-git checkout 2023.02.24
+git checkout 2023.04.15
 ./bootstrap-vcpkg.sh
 
 ./vcpkg install \
