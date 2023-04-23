531685923084b6e2a7fb6c651dc70f2919dc6afc
Onuralp Sezer <thunderbirdtr@gmail.com>
2023-04-23 21:38:37 +0300

    vcpkg version updated to 2023.04.15 (#71)
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- vcpkg_windows.bat
+++ vcpkg_windows.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build || goto :error
 git clone https://github.com/Microsoft/vcpkg.git vcpkg.windows || goto :error
 cd vcpkg.windows || goto :error
-git checkout 2023.02.24 || goto :error
+git checkout 2023.04.15 || goto :error
 call bootstrap-vcpkg.bat || goto :error
 
 vcpkg.exe install ^
